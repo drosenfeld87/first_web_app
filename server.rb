0b81c450dfbbe7cad79befe5413e1dbdb3cc7549
debbie <drosenfeld87@gmail.com>
2018-02-14 13:27:37 -0500

    got erb file working

---
+++ server.rb
@@ -0,0 +1,10 @@
+require 'sinatra'
+
+get '/home' do
+  @title = "Hi, My name is Deb"
+  @current_time = Time.now
+  @my_list = ['i like cats', 'i like pizza', 'i like heavy metal']
+
+    erb :index
+  end
+end
