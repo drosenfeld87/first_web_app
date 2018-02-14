fa39269aa4ae1c90fba221acf6c9cb9d6a2fab69
debbie <drosenfeld87@gmail.com>
2018-02-14 14:29:53 -0500

    complete first web app

--- server.rb
+++ server.rb
@@ -1,26 +1,32 @@
 require 'sinatra'
 
-get '/home' do
-  @title = "Hi, My name is Deb"
-  @current_time = Time.now
-  @my_list = ['i like cats', 'i like pizza', 'i like heavy metal']
+  get '/' do
+  'redirect to(/home)'
+  end
 
-  erb :index
-end
+  get '/home' do
+    @title = "Hi, My name is Deb"
+    @current_time = Time.now
+    @my_list = ['i like cats', 'i like pizza', 'i like heavy metal']
+
+    erb :index
+  end
 
 
-get '/portfolio' do
-  # @title = "Hi, My name is Deb"
-  # @current_time = Time.now
-  # @my_list = ['i like cats', 'i like pizza', 'i like heavy metal']
+  get '/portfolio' do
 
     erb :gallery
   end
 
   get '/aboutme' do
-    # @title = "Hi, My name is Deb"
-    # @current_time = Time.now
-    # @my_list = ['i like cats', 'i like pizza', 'i like heavy metal']
+    @skills = ['graphic design', 'css', 'html']
+    @interests = ['reading', 'art', 'music']
 
-      erb :aboutme
-    end
+    erb :aboutme
+  end
+
+  get '/favourites' do
+    @fav_links = ['<a href="reddit.com">REDDIT</a>', '<a href="debbierosenfeld.com">MY WEBSITE</a>']
+
+    erb :favourites
+  end
