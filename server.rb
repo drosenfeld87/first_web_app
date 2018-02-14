bb59d6472f02f9fb96a8acb2f470957497f7bad7
debbie <drosenfeld87@gmail.com>
2018-02-14 13:39:55 -0500

    gallery erb is working

--- server.rb
+++ server.rb
@@ -5,6 +5,14 @@ get '/home' do
   @current_time = Time.now
   @my_list = ['i like cats', 'i like pizza', 'i like heavy metal']
 
-    erb :index
-  end
+  erb :index
 end
+
+
+get '/portfolio' do
+  @title = "Hi, My name is Deb"
+  @current_time = Time.now
+  @my_list = ['i like cats', 'i like pizza', 'i like heavy metal']
+
+    erb :gallery
+  end
