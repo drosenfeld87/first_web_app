dc01489a4798fbd0fd651745698f3af6eeac6625
debbie <drosenfeld87@gmail.com>
2018-02-14 13:52:17 -0500

    got about me page working

--- server.rb
+++ server.rb
@@ -10,9 +10,17 @@ end
 
 
 get '/portfolio' do
-  @title = "Hi, My name is Deb"
-  @current_time = Time.now
-  @my_list = ['i like cats', 'i like pizza', 'i like heavy metal']
+  # @title = "Hi, My name is Deb"
+  # @current_time = Time.now
+  # @my_list = ['i like cats', 'i like pizza', 'i like heavy metal']
 
     erb :gallery
   end
+
+  get '/aboutme' do
+    # @title = "Hi, My name is Deb"
+    # @current_time = Time.now
+    # @my_list = ['i like cats', 'i like pizza', 'i like heavy metal']
+
+      erb :aboutme
+    end
